3fd642bcd98afd852cc1955bad3c06f262046d29
Tiberius Shaub <tiberiusshaub200@gmail.com>
2023-04-13 00:25:36 -0400

    button added, diables itself when pressed

--- modules/gui.py
+++ modules/gui.py
@@ -1,20 +1,55 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # Only needed for access to command line arguments
 import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.button_is_checked = True
+
+        self.setWindowTitle("CPU-DJ")
+
+        self.button = QPushButton("Generate Song")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
+
+        self.button.setFixedSize(QSize(400, 300))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+    
+    def the_button_was_clicked(self):
+        print("Song generated!")
+        self.button.setText("Song Generated.")
+        self.button.setEnabled(False)
+
+
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+
+        print(self.button_is_checked)
+
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
 app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-window = QWidget()
+# Create a Qt main window, which will be our window.
+
+window = MainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
 app.exec()
-
 
 # Your application won't reach here until you exit and the event
 # loop has stopped.
