e5e9aacea19ea2529ca2b31c1a0e47fc6c2b16ad
R-E-Miller <129326901+R-E-Miller@users.noreply.github.com>
2023-04-13 19:11:42 -0400

    eijgie

--- spotifyInteraction.py
+++ spotifyInteraction.py
@@ -33,7 +33,7 @@ def get_auth_header(token):
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f"q={artist_name}&type=artist&limit=1"
+    query = f"?q={artist_name}&type=artist&limit=1"
     
     query_url = url + query
     result = get(query_url, headers=headers)
