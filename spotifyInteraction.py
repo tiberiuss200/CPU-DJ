738b128f31652696dc5d841c177c5c5b7cc1d0ca
R-E-Miller <129326901+R-E-Miller@users.noreply.github.com>
2023-04-13 18:51:28 -0400

    wowza

--- spotifyInteraction.py
+++ spotifyInteraction.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+import base64
+from requests import post
 
 load_dotenv()
 
@@ -7,3 +9,22 @@ client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
 print(client_id, client_secret)
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
+print(token)
