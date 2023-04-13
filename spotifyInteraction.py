fe6ea350a50f46d75875da49836ce06a021e8a5a
R-E-Miller <129326901+R-E-Miller@users.noreply.github.com>
2023-04-13 18:52:08 -0400

    yep

--- spotifyInteraction.py
+++ spotifyInteraction.py
@@ -13,7 +13,7 @@ print(client_id, client_secret)
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
     
     url = "https://accounts.spotify.com/api/token"
     headers = {
