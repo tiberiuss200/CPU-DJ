5b6327ee1c8e1f9b6a1f6eff3e0c3c72e0c0dc8a
R-E-Miller <129326901+R-E-Miller@users.noreply.github.com>
2023-04-13 19:30:31 -0400

    wifjmke

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret)
-
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
