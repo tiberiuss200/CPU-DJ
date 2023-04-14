a1bce98e19a6d6c72acb3c1e58997edc806a1692
WishMakers0 <wishmakers0@github.com>
2023-04-14 09:46:19 -0400

    fixing up the async tasks

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import modules.processing
 import modules.gui
 import modules.spotify
+import asyncio
 
-cpudict = {"cpu_percent": 0.0}
-gpudict = {"filler", 0.0}
-
-def main():
-    modules.processing.main_setup(cpudict, gpudict)
+cpudict = {"cpu_percent": 0.0, "filler": 0.0}
+gpudict = {"filler": 0.0}
+asyncio.run(modules.processing.main_setup(cpudict, gpudict))
--- modules/processing.py
+++ modules/processing.py
@@ -1,25 +1,30 @@
 import asyncio
 import psutil
 
+stop_signal = False
+
 #update the CPU dictionary variable
 async def update_cpu_dict(cpudict):
-    while True:
+    while ~stop_signal:
         cpudict["cpu_percent"] = psutil.cpu_percent()
         await asyncio.sleep(1)
     print("Update CPU dictionaries task ended.")
 
 async def main_setup(cpudict, gpudict):
-    cpu_task = asyncio.create_task(update_cpu_dict(cpudict))
+    #cpu_task = asyncio.create_task()
     #gpu_task = asyncio.create_task(update_gpu_dict(gpudict))
-    debug_task = asyncio.create_task(print_dict(cpudict))
-    await debug_task
-    await cpu_task
+    #debug_task = asyncio.create_task()
+    await update_cpu_dict(cpudict)
+    await print_dict(cpudict)
     #await gpu_task
 
 async def print_dict(cpudict):
-    while True:
+    await asyncio.sleep(5)
+    while ~stop_signal:
         print(cpudict)
         await asyncio.sleep(1)
     #end
+
+
 
 #eof
