5c98153e5640f963956a3de70927a6e033387ad9
WishMakers0 <wishmakers0@github.com>
2023-04-14 09:22:29 -0400

    cpudict filler?

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import modules.processing
 import modules.gui
 import modules.spotify
+
+cpudict = {"cpu_percent": 0.0}
+gpudict = {"filler", 0.0}
+
+def main():
+    modules.processing.main_setup(cpudict, gpudict)
--- modules/processing.py
+++ modules/processing.py
@@ -11,7 +11,15 @@ async def update_cpu_dict(cpudict):
 async def main_setup(cpudict, gpudict):
     cpu_task = asyncio.create_task(update_cpu_dict(cpudict))
     #gpu_task = asyncio.create_task(update_gpu_dict(gpudict))
+    debug_task = asyncio.create_task(print_dict(cpudict))
+    await debug_task
     await cpu_task
     #await gpu_task
+
+async def print_dict(cpudict):
+    while True:
+        print(cpudict)
+        await asyncio.sleep(1)
+    #end
 
 #eof
