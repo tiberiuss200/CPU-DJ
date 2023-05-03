69375b0fb5851766abaa9cb36c05722b2c24c558
R-E-Miller <re.p.miller57@gmail.com>
2023-05-03 16:04:37 -0400

    Update state.py

--- modules/state.py
+++ modules/state.py
@@ -2,7 +2,7 @@ mainFinished = False
 mainStarted = False
 cpudict = {"cpu_percent": 0.0, "filler": 0.0}
 gpudict = {"filler": 0.0}
-emotion = "emotion"
+emotion = "emotion_default"
 
 def signalTasks():
     global mainFinished
