390fe4046bad78da3fc2636498e92daad680f3da
Nate Harris <nharr646@live.kutztown.edu>
2023-10-18 11:02:56 -0400

    Removed all Tkinter stuff

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,4 +1,4 @@
-from tkinter import *
+#from tkinter import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
@@ -7,36 +7,17 @@ import psutil
 
 
 
-#def main():
-#    graph()
-
-root = Tk()
+def main():
+    graph()
 
 
 def graph():
     ax.clear()
-    x = np.linspace(0, 10, 100)
-    y = psutil.cpu_percent() * np.sin(2 * x)
+    x = np.linspace(0.0, 10.0, 100)
+    y = psutil.cpu_percent()
     ax.plot(x, y)
 
-    canvas.draw()
-
-
-
-
 fig, ax = plt.subplots()
-frame = Frame(root)
 
-canvas = FigureCanvasTkAgg(fig, master = frame)
-canvas.get_tk_widget().pack()
-
-frame.pack()
-
-
-button=Button(root, text="Graph", command=graph)
-button.pack()
-
-root.mainloop()
-
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
