8573cfce6585b6fedf4518e492f61f7f9bdd57b1
Nate Harris <nharr646@live.kutztown.edu>
2023-10-19 17:46:22 -0400

    Added app icon and title to the graphs.py page

--- modules/graphs.py
+++ modules/graphs.py
@@ -2,11 +2,15 @@ import sys
 
 from PyQt6 import QtCore
 from PyQt6.QtWidgets import QApplication, QWidget
-
+from PyQt6.QtGui import QIcon
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowIcon(QIcon("logo.png"))
+        self.setWindowTitle("Graphs Testing")
+
+
 
 app = QApplication(sys.argv)
 window = Window()
