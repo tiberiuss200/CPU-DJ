a7c2c614f8e556d5af4de356dba908a0e54704e3
Nate Harris <nharr646@live.kutztown.edu>
2023-10-20 14:35:44 -0400

    Added test graph. Not displaying anything for now...

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,20 +1,37 @@
 import sys
 
-from PyQt6 import QtCore
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QIcon
+import matplotlib
+matplotlib.use('QtAgg')
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+class graph(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=10, height=10, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(graph, self).__init__(fig)
+
+
 
 class Window(QWidget):
     def __init__(self):
-        super().__init__()
+        super(Window, self).__init__()
         self.setWindowIcon(QIcon("logo.png"))           #Sets the window's icon in the top left of the window to our logo
         self.setWindowTitle("Graphs Testing")           #Sets the title of the entire window
+        test= graph(self, width=5, height=10, dpi=100)
+
+        test.axes.plot([0,1,2,3,4], [3,10,2,5,7])
+
+        self.show()
 
 
 
 
-def graph():
-    ...
 
 
 
