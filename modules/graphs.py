a5c79f562a159f7a2a41701177ecb87a789b000f
WishMakers0 <kirbydan830@gmail.com>
2023-10-22 22:14:22 -0400

    graph movement and embed progress

--- modules/graphs.py
+++ modules/graphs.py
@@ -1,11 +1,13 @@
 import sys
 from PyQt6 import QtCore, QtWidgets
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
 from PyQt6.QtGui import QIcon
 import matplotlib
 
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+import modules.tasks as tasks
+from psutil import cpu_percent
 
 
 
@@ -36,6 +38,44 @@ x = [0,1,2,3,4]
 y = [3,10,2,5,7]
 ax.plot(x,y)
 
+class DataGraph(QWidget):
+    def __init__(self, fptr: callable, mainWindow: QMainWindow):
+        super(DataGraph, self).__init__()
+        self.fptr = fptr
+        self.x_values = []
+        self.y_values = []
+        self.timer = 0
+        self.xmax = 10
+        self.fig = Figure()
+
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.canvas = FigureCanvasQTAgg(self.fig)
+        self.layout.addWidget(self.canvas)
+        self.init_graph()
+        tasks.start(mainWindow, self.update_graph, self)
+
+    def init_graph(self):
+        self.ax = self.fig.add_subplot()
+        self.line, = self.ax.plot([], [])
+        self.ax.set_xlim(0, 10)
+    
+    def update_graph(self, self2):
+        new_yvalue = self2.fptr()
+        self2.y_values.append(new_yvalue)
+        self2.x_values.append(self.timer)
+        self2.timer += 1
+
+        if (self2.timer > self2.xmax):
+            self2.ax.set_xlim(self2.timer - 10, self2.timer)
+        
+        self2.canvas.draw()
+        self2.line.set_data(self2.x_values, self2.y_values)
+        tasks.wait(1000)
+
+
+def test_fxn():
+    return cpu_percent()
 
 #class graph(FigureCanvasQTAgg):
 #    def __init__(self, parent=Window, width=10, height=10, dpi=100):
@@ -47,8 +87,8 @@ ax.plot(x,y)
 
 
 
-app = QApplication(sys.argv)
-window = Window()
-window.show()
+#app = QApplication(sys.argv)
+#window = DataGraph(test_fxn)  #Window()
+#window.show()
 
-sys.exit(app.exec())
+#sys.exit(app.exec())
