484e756089f1f431fdb4b667f94b8e0012a7973a
WishMakers0 <kirbydan830@gmail.com>
2023-10-24 19:19:10 -0400

    more stat tracking

--- modules/state.py
+++ modules/state.py
@@ -1,9 +1,18 @@
+import platform
+from enum import Enum
+
 mainFinished = False
 tasksStarted = False
-cpudict = {"cpu_percent": 0.0, "cpu_freq": 0.0, "ram_percent": 0.0, "swap_percent": 0.0, "filler": 0.0}
+cpudict = {"cpu_percent": 0.0, "cpu_freq": 0.0, "ram_percent": 0.0, "swap_percent": 0.0, "fan_speed": None, "temps": None, "battery_info": None, "filler": 0.0}
 gpudict = {"filler": 0.0}
 emotion = "emotion_default"
 window = None
+current_os = platform.system()
+
+class CONST_OS(Enum):
+    WIN = "Windows"
+    MAC = "Darwin"
+    LINUX = "Linux"    
 
 def signalTasks():
     global mainFinished
