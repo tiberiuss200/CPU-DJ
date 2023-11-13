ee1e37c3a33c68ec458c28ddb2e5afcef9275ba2
WishMakers0 <kirbydan830@gmail.com>
2023-11-13 14:51:44 -0500

    launcher/installer works now if venv exists

--- CPU-DJ.py
+++ CPU-DJ.py
@@ -6,19 +6,43 @@ CPU-DJ written by Daniel Oravetz, R-E Miller, Tiberius Shaub, and Nathaniel Harr
 """
 
 from venv import create as create_venv
-from os.path import join, exists, is_dir
+from os.path import join, exists
 from os import getcwd
 import subprocess
+import platform
 
-PYTHON_BIN = join(getcwd(), '.venv', 'bin', 'python')
-VENV_START_BIN = join(getcwd(), '.venv', 'bin', 'activate')
+def get_venv_bin_folder():
+    # I hate platform specific code.......
+    ret = ""
+    if(platform.system() == "Windows"):
+        ret = "Scripts"
+    else:
+        ret = "bin"
+    return ret
+
+def get_requirements_txt():
+    ret = ""
+    if (platform.system() == "Windows"):
+        ret = "require_win.txt"
+    elif (platform.system() == "Darwin"):
+        ret = "require_mac.txt"
+    else:
+        ret = "require_linux.txt"
+    return ret
+    
+VENV_BIN = get_venv_bin_folder()
+PYTHON_BIN = join(getcwd(), '.venv', VENV_BIN, 'python')
+VENV_START_BIN = join(getcwd(), '.venv', VENV_BIN, 'activate')
 VENV_DIR = join(getcwd(), '.venv')
+SCRIPT_PATH = join(getcwd(), 'main.py')
+REQ_TXT = get_requirements_txt()
 
-if (~exists(VENV_START_BIN)):
+print(VENV_START_BIN)
+
+if (not exists(VENV_START_BIN)):
     # start venv setup process
     create_venv(VENV_DIR, with_pip=True)
-    subprocess.run(["bin/pip", "install", "-r", join(getcwd(), "requirements.txt")], cwd=getcwd())
+    subprocess.run(["{VENV_BIN}/pip", "install", "-r", join(getcwd(), REQ_TXT)], cwd=getcwd())
 
-
-
-
+process = subprocess.Popen([PYTHON_BIN, SCRIPT_PATH])
+process.wait()
