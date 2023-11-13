aff4b3676474b0ab233f6bab12c16e16367eb9dd
Tiberius Shaub <tiberiusshaub200@gmail.com>
2023-11-13 11:58:27 -0500

    icon switch

--- modules/gui.py
+++ modules/gui.py
@@ -21,6 +21,8 @@ class Color(QWidget):
         super(Color, self).__init__()
         self.setAutoFillBackground(True)
 
+
+        
         palette = self.palette()
         palette.setColor(QPalette.ColorRole.Window, QColor(color))
         self.setPalette(palette)
@@ -36,7 +38,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         frameCounter = 0
         mainWindow.setWindowTitle("CPU-DJ")
-        mainWindow.setWindowIcon(QIcon('logo.png'))
+        mainWindow.setWindowIcon(QIcon('logo.ico'))
         mainWindow.setMinimumSize(200, 200)
         mainWindow.resize(1000, 600)
         mainWindow.display = ["empty"]
@@ -138,26 +140,25 @@ class MainWindow(QMainWindow):
         #create layouts
         row1 = QHBoxLayout()
 
-        #create widgets
-        container = QLabel()
-        titleBox = QLabel("CPU-DJ")
-        font = titleBox.font()
-        label = QPixmap('logo.png')
+        iconBox = QLabel()
+        logoBox = QLabel()
+        icon = QPixmap('icon.png')
+        logo = QPixmap('logo.png')
 
         #setting up variables
-        label = label.scaled(100, 100)
-        font.setPointSize(30)
+        icon = icon.scaled(100, 100)
+        logo = logo.scaled(290, 80)
 
         #applying variables
-        container.setPixmap(label)
+        iconBox.setPixmap(icon)
+        logoBox.setPixmap(logo)
 
-        titleBox.setFont(font)
-        titleBox.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
-        container.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        iconBox.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        logoBox.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
         #add widgets
-        row1.addWidget(container)
-        row1.addWidget(titleBox)
+        row1.addWidget(iconBox)
+        row1.addWidget(logoBox)
         row1.addWidget(mainWindow.moodButton)
         row1.addWidget(mainWindow.scanButton)
         row1.addWidget(mainWindow.dataButton)
