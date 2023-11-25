70301193c6ef3945fbf1b2722f69f7bf9841f407
Nate <nharr646@live.kutztown.edu>
2023-11-25 17:54:27 -0500

    fixed icon lol

--- modules/gui.py
+++ modules/gui.py
@@ -40,7 +40,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         frameCounter = 0
         mainWindow.setWindowTitle("CPU-DJ")
-        mainWindow.setWindowIcon(QIcon('logo.ico'))
+        mainWindow.setWindowIcon(QIcon('icon.png'))
         mainWindow.setMinimumSize(200, 200)
         mainWindow.resize(1000, 600)
         mainWindow.display = ["empty"]
